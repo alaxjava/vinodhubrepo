f0b28302a43c5c5ee70900732840e95592c9fc39
alaxjava <alaxjava@gmail.com>
2017-11-06 17:09:40 +0530

    commit13

--- MyWebApp/src/main/java/com/infogain/Main.java
+++ MyWebApp/src/main/java/com/infogain/Main.java
@@ -7,7 +7,7 @@ public class Main {
 		System.out.println(emp);
 		System.out.println(emp2);
 		
-		Manager manager=new Manager("vinohfkum","Insurance");
+		Manager manager=new Manager("vinohddfkum","Insurance");
 		System.out.println(manager);
 	}
 
